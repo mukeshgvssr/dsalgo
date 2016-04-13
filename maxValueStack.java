e6c5d8aab6ec196532f8883005568e8accda7820
mukeshgvssr <mukesh.gadidasu@gmail.com>
2016-04-13 21:22:09 +0530

    Create maxValueStack.java
    This is used to get the max value of the available elements in the stack in O(1)

---
+++ maxValueStack.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class maxValueStack {
+
+    public static void main(String[] args) throws IOException {
+        // TODO Auto-generated method stub
+        // Question: Can we get the max element value in the currently available in stack
+        //          in O(1) time
+       
+         StackOperations so=new StackOperations();                  
+         so.interactiveDisplay();
+         
+    }
+}
